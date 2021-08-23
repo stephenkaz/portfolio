b2756138ac63b700fe78b8cee6ace53c06a4b07a
stephenkaz <stephenkaz42@gmail.com>
2021-08-23 00:54:27 -0700

    work almost finished

--- src/components/Work.js
+++ src/components/Work.js
@@ -4,6 +4,37 @@ const Work = () => {
   return (
     <div id="work">
       <h1>Work</h1>
+
+      <div className="work__container">
+        <div className="example__1">
+          <div className="iframe__container">
+            <iframe
+              src="https://airbnb-clone-stephenkaz.vercel.app/"
+              frameborder="0"
+            ></iframe>
+          </div>
+
+          <p>
+            <a href="https://airbnb-clone-stephenkaz.vercel.app/">
+              Airbnb Clone
+            </a>
+          </p>
+        </div>
+        <div className="example__2">
+          <div className="iframe__container">
+            <iframe
+              src="https://loopstudios-landing-page-stephenkaz.vercel.app/"
+              frameborder="0"
+            ></iframe>
+          </div>
+
+          <p>
+            <a href="https://loopstudios-landing-page-stephenkaz.vercel.app/">
+              Loopstudios
+            </a>
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
