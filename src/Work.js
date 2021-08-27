5e8bc03eba6d12015adb6a764b15f4cec9c858b2
stephenkaz <stephenkaz42@gmail.com>
2021-08-27 15:49:12 -0700

    api added

--- src/Work.js
+++ src/Work.js
@@ -1,57 +1,22 @@
 import "./css/Work.css";
-import airbnbPreview from "./img/airbnb.png";
-import loopstudiosPreview from "./img/loopstudios-preview.PNG";
-import lorukiPreview from "./img/Loruki-preview.PNG";
-import statsPreview from "./img/stats-preview.PNG";
+import workJson from "./api/workData";
 
 const Work = () => {
   return (
     <>
       <h1>Work</h1>
-
       <div className="work__container">
-        <div className="example__1">
-          <div className="iframe__container">
-            <img src={airbnbPreview} alt="Airbnb" />
-          </div>
+        {workJson?.map(({ img, title, alt, href }) => {
+          return (
+            <div className="example">
+              <img src={img} alt={alt} />
 
-          <p>
-            <a href="https://airbnb-clone-stephenkaz.vercel.app/">
-              Airbnb Clone
-            </a>
-          </p>
-        </div>
-        <div className="example__2">
-          <div className="iframe__container">
-            <img src={loopstudiosPreview} alt="Loopstudios" />
-          </div>
-
-          <p>
-            <a href="https://loopstudios-landing-page-stephenkaz.vercel.app/">
-              Loopstudios
-            </a>
-          </p>
-        </div>
-        <div className="example__3">
-          <div className="iframe__container">
-            <img src={statsPreview} alt="Stats-Preview" />
-          </div>
-
-          <p>
-            <a href="https://stats-preview-stephenkaz.vercel.app/">
-              Stats-Preview
-            </a>
-          </p>
-        </div>
-        <div className="example__4">
-          <div className="iframe__container">
-            <img src={lorukiPreview} alt="Loruki" />
-          </div>
-
-          <p>
-            <a href="https://loruki-stephenkaz.vercel.app/">Loruki</a>
-          </p>
-        </div>
+              <p>
+                <a href={href}>{title}</a>
+              </p>
+            </div>
+          );
+        })}
       </div>
     </>
   );
---
+++ src/api/workData.js
@@ -0,0 +1,31 @@
+export const workData = [
+  {
+    img: "./img/airbnb.png",
+    title: "Airbnb Clone",
+    alt: "airbnb",
+    href: "https://airbnb-clone-stephenkaz.vercel.app/",
+    key: "1",
+  },
+  {
+    img: "./img/loopstudios-preview.PNG",
+    title: "Loopstudios",
+    alt: "loopstudios",
+    href: "https://loopstudios-landing-page-stephenkaz.vercel.app/",
+    key: "2",
+  },
+  {
+    img: "./img/stats-preview.PNG",
+    title: "Stats-Preview",
+    alt: "stats preview",
+    href: "https://stats-preview-stephenkaz.vercel.app/",
+    key: "3",
+  },
+  {
+    img: "./img/Loruki-preview.PNG",
+    title: "Loruki",
+    alt: "loruki",
+    href: "https://loruki-stephenkaz.vercel.app/",
+    key: "4",
+  },
+];
+export default workData;
